2df3c31b9f328023890049dbaca7138685b11c52
Jordan Prescott <jprescott23@gmail.com>
2023-10-24 09:31:40 +0100

    scripter object holds api for scripts

--- odin_api/scripter.py
+++ odin_api/scripter.py
@@ -1,6 +1,14 @@
 # user will use this module as an entry to oa_scripts for example:
 # odin_aip.scripts.find_alias('foo') will call the relevant script in oa_scipts = oa_scripts.find_alias('foo')
+import scripts
 
 class Scripter:
-    def __init__(self) -> None:
-        pass
+    """ scripter
+
+    :param api: 
+    """
+    def __init__(self, api) -> None:
+        self.api = api
+
+    def find_alias(self, api, group, alias):
+        return scripts.find_alias.main(api, group, alias)
