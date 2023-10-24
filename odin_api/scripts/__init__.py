0b550aaf8f71882b6d08ed8cfbe19d6b84354bec
Jordan Prescott <jprescott23@gmail.com>
2023-10-24 09:32:17 +0100

    scripts package structure update

---
+++ odin_api/scripts/__init__.py
@@ -0,0 +1,15 @@
+__all__ = [
+    "bulk_enable_voicemail",
+    "bulk_password_reset",
+    "find_alias",
+    "group_audit",
+    "user_activity",
+    "user_huntgroup_membership"
+]
+
+from .bulk_enable_voicemail import *
+from .bulk_password_reset import *
+from .find_alias import *
+from .group_audit import *
+from .user_activity import *
+from .user_huntgroup_membership import *
