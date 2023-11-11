f859c3625005a451a4376b9a731dde47b269201a
Jordan-Prescott <Jprescott23@gmail.com>
2023-11-11 12:34:18 +0000

    time added for logs

--- odin_api/utils/generators.py
+++ odin_api/utils/generators.py
@@ -1,5 +1,9 @@
+from datetime import datetime
 
 def generate_password(length=8):
     characters = string.ascii_letters + string.digits + string.punctuation
     password = ''.join(secrets.choice(characters) for _ in range(length))
     return password
+
+def generate_timestamp():
+    return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
