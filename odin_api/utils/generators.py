2eed2d60a432d81ff4990662625dfdfd0b22b52c
Jordan-Prescott <Jprescott23@gmail.com>
2023-11-14 20:19:59 +0000

    Update generators.py

--- odin_api/utils/generators.py
+++ odin_api/utils/generators.py
@@ -1,9 +1,22 @@
 from datetime import datetime
 
 def generate_password(length=8):
+    """_summary_
+
+    Parameters:
+        length (int, optional): _description_. Defaults to 8.
+
+    Returns:
+        _type_: _description_
+    """
     characters = string.ascii_letters + string.digits + string.punctuation
     password = ''.join(secrets.choice(characters) for _ in range(length))
     return password
 
 def generate_timestamp():
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """
     return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
