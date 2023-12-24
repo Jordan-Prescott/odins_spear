334021e10d00861d07bed47ba128e870fb1dbe8c
Jordan-Prescott <Jprescott23@gmail.com>
2023-12-24 14:30:57 +0000

    singleton pattern doesn't work
    If user would like to have multiple APIs for regions the singleton pattern would enforce only one connection.

--- odin_api/requester.py
+++ odin_api/requester.py
@@ -1,27 +1,15 @@
 
 class requester():
     
-    __instance = None
-
-    @staticmethod
-    def get_instance():
-        if requester.__instance is None:
-            requester()
-        return requester.__instance
-    
     def __init__(self, base_url, username, password):
-        if requester.__instance is not None:
-            raise Exception("Singleton cannot be instantiated more than once!")
-        else:
-            self.base_url = base_url
-            self.username = username
-            self.password = password
-            self.headers = {
-                'Authorization': '',
-                'Content-Type': 'application/json'
-            }
-            self.token = ""
+        self.base_url = base_url
+        self.username = username
+        self.password = password
+        self.headers = {
+            'Authorization': '',
+            'Content-Type': 'application/json'
+        }
+        self.token = ""
         
-        requester.__instance = self
     
     
