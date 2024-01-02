ee3e9398a6d5e32b0a2da8b09cea8dde9fabdc87
Jordan Prescott <jprescott23@gmail.com>
2024-01-02 11:48:39 +0000

    Adjusted removing name

--- odin_api/logger.py
+++ odin_api/logger.py
@@ -2,7 +2,7 @@ import logging
 
 # configuration
 logging.basicConfig(level=logging.INFO, 
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+                    format='%(asctime)s - %(levelname)s - %(message)s')
 
 logger = logging.getLogger()
 
