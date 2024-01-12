c6bab8cd2063f4cbc108e46967cab05d125679c4
Jordan Prescott <jprescott23@gmail.com>
2024-01-12 11:50:11 +0000

    new excpetion for invalid codes

--- odin_api/exceptions.py
+++ odin_api/exceptions.py
@@ -70,3 +70,10 @@ class AOFailedToLocateSession(OAError):
 
     def __str__(self) -> str:
         return f"Session details not found. Check token is valid and not exppired."
+    
+class AOInvalidCode(OAError):
+    """ Raised when code is less than 4 and higher than 6.
+    """
+
+    def __str__(self) -> str:
+        return f"Code needs to be between 4 and 6 digits."
