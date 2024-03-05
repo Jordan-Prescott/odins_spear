200c4c133e2e8f02de04b935d9d3bc7f158d6fc1
Jordan Prescott <jprescott23@gmail.com>
2024-03-05 13:00:24 +0000

    Move numbers can now move single numbers across enterprises
    Next is multiple numbers

--- odins_spear/scripts/move_numbers.py
+++ odins_spear/scripts/move_numbers.py
@@ -5,6 +5,7 @@ import odins_spear.logger as logger
 def main(api, current_service_provider_id: str, current_group_id: str, target_service_provider_id: str, 
          target_group_id: str, start_of_range_number: str, end_of_range_number: str = None) -> bool:
     
+    logger.log_info(f"Removing numbers from {current_group_id}.")
     # delete number from group
     api.delete.group_dns(
         current_service_provider_id,
@@ -16,6 +17,7 @@ def main(api, current_service_provider_id: str, current_group_id: str, target_se
     # check if sp/ent are the same if not number needs to be removed to sys level
     if not current_service_provider_id == target_service_provider_id:
         
+        logger.log_info(f"Removing numbers from {current_service_provider_id}.")
         # remove from sp/ent
         api.delete.service_provider_dns(
             current_service_provider_id,
@@ -23,15 +25,9 @@ def main(api, current_service_provider_id: str, current_group_id: str, target_se
             end_of_range_number = end_of_range_number
         )
         
-        # assign to new sp/ent
-        api.post.service_provider_dns(
-            target_service_provider_id,
-            start_of_range_number = start_of_range_number,
-            end_of_range_number = end_of_range_number
-        )
-        
+        logger.log_info(f"Adding number to {target_service_provider_id} > {target_group_id}.")
         # assign to new group
-        api.post.group_dns(
+        api.post.group_dns_assign_bulk(
             target_service_provider_id,
             target_group_id,
             start_of_range_number = start_of_range_number,
@@ -40,6 +36,7 @@ def main(api, current_service_provider_id: str, current_group_id: str, target_se
     else:
     # only when group is in the same sp/ent    
         
+        logger.log_info(f"Adding number to {target_group_id}.")
         # assign to new group
         api.post.group_dns(
             target_service_provider_id,
