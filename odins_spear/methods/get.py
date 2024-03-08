b1dd76a795a7ad61ce5005fa5268c070d70fd6f2
Jordan Prescott <jprescott23@gmail.com>
2024-03-08 09:30:30 +0000

    registration added

--- odins_spear/methods/get.py
+++ odins_spear/methods/get.py
@@ -459,6 +459,37 @@ class Get():
 # PUSH REGISTRATION
 # PUSH TO TALK
 # REGISTRATION
+
+    def user_registration(self, user_id: str):
+        """Gets a users devices and if those devices are registered. This includes soft phones.
+
+        Args:
+            user_id (str): Target user ID to check registration
+
+        Returns:
+            dict: All users devices and details on device such as registration.
+        """
+        
+        endpoint = f"/users/registration/?userId={user_id}"
+
+        return self.requester.get(endpoint)
+    
+    
+    def bulk_user_registration(self, service_provider_id: str, group_id: str):
+        """Gets all users in a group and their device registrations. This includes soft phones.
+
+        Args:
+            service_provider_id (str): Service Provider/ Enterprise ID where Group is hosted. 
+            group_id (str): Target Group ID where users are located. 
+
+        Returns:
+            dict: All users devices and details on device such as registration.
+        """
+        
+        endpoint = f"/users/registration/bulk?serviceProviderId={service_provider_id}&groupId={group_id}"
+
+        return self.requester.get(endpoint)
+
 # REMOTE OFFICE
 # REPORTS
 
