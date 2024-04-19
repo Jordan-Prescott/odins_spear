2935c7886c4be40314d47ed0ce22ee5e9fbf4789
Jordan Prescott <jprescott23@gmail.com>
2024-04-19 10:56:28 +0100

    utilised loguru and desired behaviour is now working
    Need to tidy up the class and work on user experience. Once complete I can doc things and push new release.

--- odins_spear/logger.py
+++ odins_spear/logger.py
@@ -1,48 +1,41 @@
-import logging
-
-from logging.handlers import SysLogHandler, RotatingFileHandler
-from logging import FileHandler
+from loguru import logger
 
 class Logger:
-    
     def __init__(self, user: str = None) -> None:
 
         self._user = user
-            
-        self._logger_obj = logging.getLogger('os')
-        self._logger_obj.setLevel(logging.DEBUG)
-        self._formatter = logging.Formatter('%(asctime)s - %(name)s - %(message)s - %(user)s - %(endpoint)s - %(response_code)s')
-        
-        self._logger_obj.handlers.clear()
+             
+        self._logger_obj = logger
+        self._logger_obj.remove() 
         
         self.set_up_file_handler()
     
     
-    def set_up_file_handler(self):
-            
-        log_file = 'app.log'
-        backup_count = 5
+    def set_up_file_handler(self, path='app.log'):
+          
+        self._logger_obj.add(
+            path,
+            format="{time} | {message} | {extra}"
+        )
+                
         
-        handler = RotatingFileHandler(log_file, backupCount=backup_count)
-        handler.setFormatter(self._formatter)
-        
-        self._logger_obj.addHandler(handler)
-        
-        
-    def set_up_sys_log_handler(self, url: str, port_number: int):
+    def set_up_sys_log_handler(self, url: str):
         
         self.url = url
-        self.port_number = port_number
         
-        self._logger_obj.addHandler(SysLogHandler(address=(self.url, self.port_number)))
+        self._logger_obj.add(
+            self.url,
+            format="{time} | {message} | {extra}"
+        )
         
         
-    def log_request(self, endpoint: str, response_code: int):
-        self._logger_obj.info(
-            "LOG", 
-            extra={
-                'user': self._user, 
-                'endpoint': endpoint,
-                'response_code': response_code}
+    def _log_request(self, endpoint: str, response_code: int):
+        with self._logger_obj.contextualize(
+            user=self._user, 
+            endpoint=endpoint, 
+            response_code=response_code):
+            
+            self._logger_obj.info(
+                "odins_spear"
             )
         
