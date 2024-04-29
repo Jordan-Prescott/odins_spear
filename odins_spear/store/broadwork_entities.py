e380383077471553b21f779681044871b51805b3
Jordan-Prescott <Jprescott23@gmail.com>
2024-04-29 17:01:34 +0100

    Update broadwork_entities.py

--- odins_spear/store/broadwork_entities.py
+++ odins_spear/store/broadwork_entities.py
@@ -9,17 +9,6 @@ class ServiceProvider:
     name: str
     groups: List['Group'] = field(default_factory=list)
     is_enterprise: bool = False
-    default_domain: str = None
-    support_email: str = None
-    contact_name: str = None
-    contact_number: str = None
-    contact_email: str = None
-    address_line1: str = None
-    city: str = None
-    state_or_province: str = None
-    zip_or_postcode: str = None
-    country: str = None
-    use_service_provider_language: bool = False
    
     
 @dataclass(kw_only=True)
@@ -28,22 +17,8 @@ class Group:
     id: str
     name: str
     default_domain: str
-    user_limit: int = None
-    user_count: int = None
-    calling_line_id_name: str = None
     calling_line_id_phone_number: str = None
-    calling_line_id_display_phone_number: str = None
-    time_zone: str = None
-    time_zone_display_name: str = None
-    location_dialing_code: str = None
-    contact_name: str = None
-    contact_number: str = None
-    contact_email: str = None
-    address_line1: str = None
-    address_line2: str = None
-    city: str = None
-    state_or_province: str = None
-    country: str = None
+    auto_attendants: List['AutoAttendant'] = field(default_factory=list)
     trunk_groups: List['TrunkGroup'] = field(default_factory=list)
     call_centers: List['CallCenter'] = field(default_factory=list)
     hunt_groups: List['HuntGroup'] = field(default_factory=list)
@@ -57,55 +32,19 @@ class Group:
         
 @dataclass(kw_only=True)
 class TrunkGroup:
-    name: str
+    service_user_id: str
     group: Type['Group']
-    access_device: Type['Device']
     users: List['User'] = field(default_factory=list)
-    allow_termination_to_dtg_identity: bool = False
-    allow_termination_to_trunk_group_identity: bool = False
-    allow_unscreened_calls: bool = False
-    allow_unscreened_emergency_calls: bool = False
-    capacity_exceeded_trap_initial_calls: int = None
-    capacity_exceeded_trap_offset_calls: int = None
-    clid_source_for_screened_calls_policy: str = None
-    continuous_options_sending_interval_seconds: int = None
-    enable_bursting: bool = False
-    enable_network_address_identity: bool = False
-    failure_options_sending_interval_seconds: int = None
-    failure_threshold_counter: int = None
-    include_dtg_identity: bool = False
-    include_otg_identity_for_network_calls: bool = False
-    include_trunk_group_identity: bool = False
-    include_trunk_group_identity_for_network_calls: bool = False
-    invitation_timeout: int = None
-    invite_failure_threshold_counter: int = None
-    invite_failure_threshold_window_seconds: int = None
-    pilot_user_call_optimization_policy: str = None
-    pilot_user_calling_line_asserted_identity_policy: str = None
-    pilot_user_calling_line_identity_for_emergency_calls_policy: str = None
-    pilot_user_calling_line_identity_for_external_calls_policy: str = None
-    pilot_user_charge_number_policy: str = None
-    prefix_enabled: bool = False
-    require_authentication: bool = False
-    route_to_peering_domain: bool = False
-    send_continuous_options_message: bool = False
-    stateful_rerouting_enabled: bool = False
-    success_threshold_counter: int = None
-    use_system_clid_source_for_screened_calls_policy: bool = False
-    use_system_calling_line_asserted_identity_policy: bool = False
-    use_system_user_lookup_policy: bool = False
-    user_lookup_policy: str = None
     max_active_calls: int = None
-    max_incoming_calls: int = None
-    max_outgoing_calls: int = None
+    bursting_max_active_calls: bool = False
 
     def __post_init__(self):
-        self.service_provider_id = self.group.service_provider_id.id
+        self.group.trunk_groups.append(self)
 
 
 @dataclass(kw_only=True)
 class AAKey:
-    key_number: int
+    number: int
     action: str
     description: str = None
     phone_number: str = None
@@ -114,98 +53,78 @@ class AAKey:
 
 @dataclass(kw_only=True)
 class AAMenu:
-    announcement_selection: str = None
     enable_first_menu_level_extension_dialing: bool = False
     keys: List[AAKey] = field(default_factory=list)
 
 
 @dataclass(kw_only=True)
 class AutoAttendant:
+    service_user_id: str
     name: str
     group: Type['Group']
-    calling_line_id_last_name: str = None
-    calling_line_id_first_name: str = None
-    hiragana_last_name: str = None
-    hiragana_first_name: str = None
-    language: str = None
-    time_zone: str = None
-    time_zone_display_name: str = None
+    extension: str = None
+    phone_number: str = None
     aliases: List[str] = field(default_factory= [list])
     type: str = None
-    enable_video: bool = False
-    extension_dialing_scope: str = None
-    name_dialing_scope: str = None
-    name_dialing_entries: str = None
     business_hours_menu: Type['AAMenu'] = None
     after_hours_menu: Type['AAMenu'] = None
-    service_user_id: str = None
+    
 
     def __post_init__(self):
-        self.serviceProviderId = self.group.ServiceProvider.id
+        self.group.auto_attendants.append(self)
         
 
 @dataclass(kw_only=True)
 class CallCenter:
-    name: str
+    service_user_id: str
     group: Type['Group']
     agents: List['User'] = field(default_factory=list)
-    enable_video: bool = False
-    allow_caller_to_dial_escape_digit: bool = False
-    reset_call_statistics_upon_entry_in_queue: bool = True
-    allow_agent_logoff: bool = True
-    allow_call_waiting_for_agents: bool = True
-    play_ringing_when_offering_call: bool = True
-    external_preferred_audio_codec: str = None
-    internal_preferred_audio_codec: str = None
-    enable_reporting: bool = False
-    allow_calls_to_agents_in_wrap_up: bool = True
-    override_agent_wrap_up_time: bool = False
-    enable_automatic_state_change_for_agents: bool = False
-    force_delivery_of_calls: bool = False
+    extension: str = None
+    phone_numner: str = None
+    name: str = None
     type: str = None
-    service_user_id_prefix: str = None
-    calling_line_id_last_name: str = None
-    calling_line_id_first_name: str = None
-    password: str = field(default_factory=gen.generate_password)
     policy: str = None
-    routing_type: str = None
-    queue_length: int = None
-    escape_digit: str = None
-    service_user_id: str = None
-
+   
+    bounced_calls_enabled: bool = False
+    bounced_calls_transfer_to_phone_number: bool = False
+    overflow_calls_action: str = None
+    overflow_calls_transfer_to_phone_number: bool = False
+    stranded_calls_action: str = None
+    stranded_calls_transfer_to_phone_number: bool = False
+    stranded_call_unavailable_action: str = None
+    stranded_call_unavailable_transfer_to_phone_number: bool = False
+    
+    #NOTE: Not sure which forwarding this is.
+    forced_forwarding_enabled: bool = False
+    forced_forwarding_forward_to_phone_number: str = None
+    
+    night_service: str = None
+    holiday_service: str = None
+    
     def __post_init__(self):
-        self.service_provider_id = self.group.service_provider_id.id
         self.group.call_centers.append(self)
 
 
 @dataclass(kw_only=True)
 class HuntGroup:
+    service_user_id: str
     name: str
     group: Type['Group']
     agents: List['User'] = field(default_factory=list)
-    calling_line_id_last_name: str = None
-    calling_line_id_first_name: str = None
-    hiragana_last_name: str = None
-    hiragana_first_name: str = None
-    language: str = None
-    time_zone: str = None
     aliases: List[str] = field(default_factory=list)
+    extension: str = None
+    phone_number: str = None
     policy: str = None
-    hunt_after_no_answer: bool = None
-    no_answer_number_of_rings: int = None
-    forward_after_timeout: bool = None
+    
+    forward_after_timeout_enabled: bool = False
     forward_timeout_seconds: int = None
-    allow_call_waiting_for_agents: bool = None
-    use_system_hunt_group_clid_setting: bool = None
-    include_hunt_group_name_in_clid: bool = None
-    enable_not_reachable_forwarding: bool = None
-    make_busy_when_not_reachable: bool = None
-    service_user_id: str = None
-    service_provider_id: str = None
-    group_id: str = None
+    no_answer_number_of_rings: int = None
+    no_answer_forward_to_phone_number: str = None
+    
+    call_forward_not_reachable_enabled: bool = False
+    call_forward_not_reachable_transfer_to_phone_number: str = None
     
     def __post_init__(self):
-        self.service_provider_id = self.group.service_provider_id.id
         self.group.hunt_groups.append(self)
     
         
@@ -216,95 +135,13 @@ class User:
     first_name: str = None
     last_name: str = None
     extension: str = None
-    access_device_endpoint: Type['Device'] = None
-    calling_line_id_last_name: str = None
-    calling_line_id_first_name: str = None
-    hiragana_last_name: str = None
-    hiragana_first_name: str = None
     phone_number: str = None
-    calling_line_id_phone_number: str = None
-    password: str = None
-    department: Type['Department'] = None
-    department_full_path: str = None
-    language: str = None
-    time_zone: str = None
-    time_zone_display_name: str = None
-    default_alias: str = None
-    title: str = None
-    pager_phone_number: str = None
-    mobile_phone_number: str = None
-    email_address: str = None
-    yahoo_id: str = None
-    address_location: str = None
-    address: Type['Address'] = None
-    country_code: str = None
-    network_class_of_service: str = None
-    allow_video: bool = True
-    domain: str = None
-    endpoint_type: str = None
     aliases: List[str] = field(default_factory=list)
-    trunk_addressing: str = None
-    alternate_user_id: List[str] = field(default_factory=list)
-    is_enterprise: bool = False
-    password_expires_days: int = 2147483647
-    service_provider_id: str = None
-    line_port: str = None
+
 
     def __post_init__(self):
-        self.is_enterprise = self.group.service_provider.is_enterprise
-        self.service_provider_id = self.group.service_provider.id
         self.group.users.append(self)
-        self.id = self.id + self.group.default_domain
-        self.group_id = self.group.id
  
-    
-@dataclass(kw_only=True)
-class Device:
-    device_type: str
-    name: str
-    group: Type['Group']
-    device_level: Type['Group']
-    use_custom_user_name_password: bool = True
-    access_device_credential_name: str = None
-    access_device_credential_password: str = None
-    net_address: str = None
-    port: str = None
-    outbound_proxy_server_net_address: str = None
-    stun_server_net_address: str = None
-    mac_address: str = None
-    serial_number: str = None
-    description: str = None
-    physical_location: str = None
-    transport_protocol: str = None
-    profile: str = None
-    static_registration_capable: str = None
-    config_type: str = None
-    protocol_choice: List[str] = field(default_factory=list)
-    is_ip_address_optional: bool = True
-    use_domain: bool = True
-    is_mobility_manager_device: bool = False
-    device_configuration_option: str = None
-    static_line_ordering: bool = False
-    device_type_level: str = None
-    tags: List[str] = field(default_factory=list)
-    related_services: List[str] = field(default_factory=list)
-    protocol: str = None
-    user_name: str = None
-    group_id: int = field(init=False)
-    service_provider_id: int = field(init=False)
-    
-    # USER HAS THIS OVER DEVICE
-    # contacts 
-    # support_visual_device_management
-    # number_of_ports
-    # number_of_assigned_ports 
-    # status
-    # line_port
-
-    def __post_init__(self):
-        self.group_id = self.group.id
-        self.service_provider_id = self.group.service_provider.id
-        
         
 @dataclass(kw_only=True)
 class Contact:
