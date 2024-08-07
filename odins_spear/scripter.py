b03dab7435c7ef344187431f702ba202f335c596
CHOPP3D <64507146+Chopped4Life@users.noreply.github.com>
2024-08-07 13:34:06 +0100

    Group Audit

--- odins_spear/scripter.py
+++ odins_spear/scripter.py
@@ -1,5 +1,5 @@
 from . import scripts
-
+from .utils import parsing
 
 class Scripter:
     """ This object acts as the gateway to all pre-written scripts in /scripts/.
@@ -88,7 +88,7 @@ class Scripter:
         Returns:
             str: A JSON formatted report of the group.
         """
-        return scripts.group_audit.main(self.api, service_provider_id, group_id)
+        return parsing.json_to_dictionary(scripts.group_audit.main(self.api, service_provider_id, group_id))
 
 
     def service_pack_audit(self, servive_provider_id, group_id):
