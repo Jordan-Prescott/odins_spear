ab1a49f6b0891eaea13cb3dcb17cf6b9ad05322f
Jordan Prescott <jprescott23@gmail.com>
2024-09-04 14:25:35 +0100

    feature set up for dev
    - Added to scripts folder
    - Updated __init__
    - Write docstring for metho

---
+++ odins_spear/scripts/webex_builder.py
@@ -0,0 +1,26 @@
+
+def main():
+    
+    # 1. update the email - KB
+    
+    # 2. update alt user id - KB
+    
+    # 3. assign feature pack - OC
+    
+    # 4. enable IMP in service settings (Micheal Clarke 04.09.24) - MC
+    
+    # 5. build device - OC
+        
+    # 6. add device to user based on primary flag - JP
+    
+    # 7. get webex password - JP
+    
+    # 8. return formatted data
+    '''
+    username - webex
+    password - webex
+    primary_device
+    device_type 
+    '''
+    
+    pass
