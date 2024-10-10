76e2fd7959ab81992402fe7520a87631de7543b3
Malkin0xb <michaelclarkey26@gmail.com>
2024-10-10 19:48:43 +0100

    Update delete.py

--- odins_spear/methods/delete.py
+++ odins_spear/methods/delete.py
@@ -230,6 +230,26 @@ class Delete():
 #TRUNK GROUPS
 #TWO STAGE DIALING
 #USERS
+
+def user(self, user_id: str):
+
+        """Deletes the specified User
+
+        Args:
+            user_id (str): The target user ID to be deleted.
+
+        Returns:
+            Dict: Profile of the deleted User.
+
+        """
+
+        endpoint = "/users"
+
+        params = {
+            "userId": user_id
+        }
+
+        return self.requester.delete(endpoint, params=params)
 #USER CUSTOM RINGBACK
 #VIDEO ADD ON
 #VIRTUAL ON-NET ENTERPRISE EXTENSIONS
