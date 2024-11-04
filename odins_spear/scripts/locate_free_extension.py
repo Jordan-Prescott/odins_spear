edb8861956909bc86cda9f497766a33f9120b0ae
CHOPP3D <64507146+Chopped4Life@users.noreply.github.com>
2024-11-04 15:07:13 +0000

    Update locate_free_extension.py

--- odins_spear/scripts/locate_free_extension.py
+++ odins_spear/scripts/locate_free_extension.py
@@ -1,1 +1,11 @@
+# 1. Fetching User Information: Retrieve all users within a specified service provider or group.
+#Get all active Users, Alias, HG, CC and AA in a group.
+#api.get.users(), api.scripter.find_alias(), api.get.group_hunt_groups(), api.get.group_call_centers(), api.get.auto_attendants()
+#Will require ServiceProvider_id, Group_id
+
+
+# 2. Filtering Available Extensions: Detect unassigned or "free" extensions.
+#Filter from provided range.
+
+# 3. Output Free Extensions: Likely an output in terminal showing all exts available. If in an easily format to be used in other script.
 
