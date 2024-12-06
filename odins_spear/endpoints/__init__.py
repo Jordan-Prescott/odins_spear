771e8ae0ffd9725f7b623d96170c0d8968010754
Malkin0xb <michaelclarkey26@gmail.com>
2024-12-06 13:23:32 +0000

    Added Alternate Numbers

--- odins_spear/endpoints/__init__.py
+++ odins_spear/endpoints/__init__.py
@@ -1,11 +1,13 @@
 # endpoints/__init__.py
 
 from .administrators import Administrators
+from .alternate_numbers import AlternateNumbers
 from .call_records import CallRecords
 from .users import Users
 
 __all__ = [
     "Administrators",
+    "AlternateNumbers",
     "CallRecords",
     "Users",
 ]
