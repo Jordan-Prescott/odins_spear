ad2767896f673eef666975726da31a1a1124b3e6
Jordan Prescott <Jprescott23@gmail.com>
2024-12-06 16:23:20 +0000

    mir-call-forwarding-always

--- odins_spear/endpoints/__init__.py
+++ odins_spear/endpoints/__init__.py
@@ -1,11 +1,13 @@
 # endpoints/__init__.py
 
 from .administrators import Administrators
+from .call_forwarding_always import CallForwardingAlways
 from .call_records import CallRecords
 from .users import Users
 
 __all__ = [
     "Administrators",
+    "CallForwardingAlways",
     "CallRecords",
     "Users",
 ]
