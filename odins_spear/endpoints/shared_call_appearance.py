acbff0ba8b94d5b95713af260815cb554f354e3c
Malkin0xb <michaelclarkey26@gmail.com>
2024-12-10 11:01:39 +0000

    remove text

--- odins_spear/endpoints/shared_call_appearance.py
+++ odins_spear/endpoints/shared_call_appearance.py
@@ -5,14 +5,6 @@ class SharedCallAppearance(BaseEndpoint):
     def __init__(self):
         super().__init__()
 
-
-# TODO: copy and paste the methods from get, post, put, delete.py her in the correct section below.
-# TODO: renam methods including the method call. Example: def group_admin -> def post_group_admin
-# TODO: adjust the requester method call in each methods return. Change from self.requester{method} to self._requester{method}
-
-# NOTE: as an example to follow look at endpoitns/administrators.py
-# NOTE: delete these comments after.
-
 # GET
 
 # POST
