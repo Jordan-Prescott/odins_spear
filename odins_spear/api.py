7e732b588a690f1bb4956557fd6ed2003b2f6d88
Jordan <jprescott23@gmail.com>
2024-12-10 10:11:24 +0000

    mir-password-generate

--- odins_spear/api.py
+++ odins_spear/api.py
@@ -53,6 +53,7 @@ class API:
         self.call_records = CallRecords()
         self.dns = DNs()
         self.hunt_groups = HuntGroups()
+        self.password_generate = PasswordGenerate()
         self.users = Users()
 
     def authenticate(self) -> bool:
