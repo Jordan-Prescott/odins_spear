99fe2a3cad6f97359743441fc0e27b343c1feef4
Jordan Prescott <Jprescott23@gmail.com>
2024-12-10 15:37:45 +0000

    mir-call-forwarding-not-reachable (#162)
    Co-authored-by: Dan <57908844+dan-writes-code@users.noreply.github.com>

--- odins_spear/endpoints/__init__.py
+++ odins_spear/endpoints/__init__.py
@@ -3,6 +3,7 @@ from .alternate_numbers import AlternateNumbers
 from .auto_attendant import AutoAttendants
 from .call_centers import CallCenters
 from .call_forwarding_always import CallForwardingAlways
+from .call_forwarding_not_reachable import CallForwardingNotReachable
 from .call_forwarding_no_answer import CallForwardingNoAnswer
 from .call_forwarding_busy import CallForwardingBusy
 from .call_records import CallRecords
@@ -16,6 +17,7 @@ __all__ = [
     "AutoAttendants",
     "CallCenters",
     "CallForwardingAlways",
+    "CallForwardingNotReachable",
     "CallForwardingNoAnswer",
     "CallForwardingBusy",
     "CallRecords",
