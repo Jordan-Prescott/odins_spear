c33a08a91f039a1409ae981b3d7ead353de0d0dc
Jordan <jprescott23@gmail.com>
2024-12-11 10:44:50 +0000

    Typing and memory eff added

--- odins_spear/utils/formatters.py
+++ odins_spear/utils/formatters.py
@@ -1,7 +1,9 @@
+from typing import List, Dict, Any
+
 from .checkers import check_type_filter
 
 
-def format_filter_value(filter_type, filter_value):
+def format_filter_value(filter_type: str, filter_value: str) -> str:
     """Takes in a filter type and the value to filter for. Depenining on the type
     the value is formatted with the correct wild card e.g. 'contains' will add a
     wildcard to the start and end of value *value*
@@ -13,8 +15,7 @@ def format_filter_value(filter_type, filter_value):
     Raises:
         OAUnsupportedFilter: Raised when unsupported filter is requested such as 'ends with'
 
-    Returns:
-        str: Formatted filter with the value and correct filter wildcards.
+    Returns: Formatted filter with the value and correct filter wildcards.
     """
 
     filter_type = filter_type.lower()
@@ -28,7 +29,7 @@ def format_filter_value(filter_type, filter_value):
         return f"*{filter_value}*"
 
 
-def format_int_list_of_numbers(counrty_code: int, numbers: list):
+def format_int_list_of_numbers(counrty_code: int, numbers: List[int]) -> List[str]:
     """Takes a list of integer numbers with no country code and the country code needed.
     This will then return a list of strings with the country code inserted infront of all
     numbers in orginal list.
@@ -37,19 +38,18 @@ def format_int_list_of_numbers(counrty_code: int, numbers: list):
         counrty_code (int): Country code to be added infront of all numbers in list
         numbers (list): List of integer numbers with no country code
 
-    Returns:
-        list: List of numbers in string format with the country code added.
+    Returns: List of numbers in string format with the country code added.
     """
-    numbers.sort()
-    formatted_numbers = [f"+{str(counrty_code)}-{str(num)}" for num in numbers]
-    return formatted_numbers
+    return [f"{counrty_code}-{number}" for number in sorted(numbers)]
 
 
-def format_service_instance_profile(data: dict):
+def format_service_instance_profile(data: Dict) -> Dict[str, Any]:
     """Adds a blank dict if serviceInstanceProfile is not in data but needed
 
     Args:
         data (dict): Data to check if serviceInstanceProfile is in
+
+    Returns: Empty Dict with serviceInstanceProfile key
     """
 
     return data.setdefault("serviceInstanceProfile", {})
