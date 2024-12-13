40d6d9efa72d65bfa7134b83a27f9fdb87b57fd7
Malkin0xb <michaelclarkey26@gmail.com>
2024-12-13 13:00:00 +0000

    mir-authentication

--- odins_spear/endpoints/__init__.py
+++ odins_spear/endpoints/__init__.py
@@ -1,5 +1,6 @@
 from .administrators import Administrators
 from .alternate_numbers import AlternateNumbers
+from .authentication import Authentication
 from .auto_attendant import AutoAttendants
 from .call_centers import CallCenters
 from .call_forwarding_always import CallForwardingAlways
@@ -27,6 +28,7 @@ from .users import Users
 __all__ = [
     "Administrators",
     "AlternateNumbers",
+    "Authentication",
     "AutoAttendants",
     "CallCenters",
     "CallForwardingAlways",
---
+++ odins_spear/endpoints/authentication.py
@@ -0,0 +1,52 @@
+from .base_endpoint import BaseEndpoint
+
+
+class Authentication(BaseEndpoint):
+    def __init__(self):
+        super().__init__()
+
+
+# GET
+
+# POST
+
+# PUT
+
+
+def user_authentication_service(self, user_id: str, new_password: str):
+    """Set new SIP Authentication password for a single user.
+
+    Args:
+        user_id (str): Target user ID to reset the SIP authentication password.
+        new_password (str): New SIP authentication password to apply to new user.
+
+    Returns:
+        None: This method does not return any specific value.
+    """
+
+    endpoint = "/users/authentication"
+
+    data = {"userId": user_id, "newPassword": new_password}
+
+    return self._requester.put(endpoint, data=data)
+
+
+def user_web_authentication_password(self, user_id: str, new_password: str):
+    """Set new Web Authentication password for a single user.
+
+    Args:
+        user_id (str): Target user ID to reset the web authentication password.
+        new_password (str): New web authentication password to apply to new user.
+
+    Returns:
+        None: This method does not return any specific value.
+    """
+
+    endpoint = "/users/passwords"
+
+    data = {"userId": user_id, "newPassword": new_password}
+
+    return self._requester.put(endpoint, data=data)
+
+
+# DELETE
