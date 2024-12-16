a852ad813d0fc108081ef888fd35dba9a1eef194
Jordan <jprescott23@gmail.com>
2024-12-16 15:05:31 +0000

    mir-scripter-bulk-password-reset

--- odins_spear/scripts/bulk_password_reset.py
+++ odins_spear/scripts/bulk_password_reset.py
@@ -1,62 +1,63 @@
 from ..exceptions import OSInvalidPasswordType
 
 
-def main(api, service_provider_id: str, group_id: str, users: list, password_type: str):
+def main(
+    api, service_provider_id: str, group_id: str, users: list, password_type: str
+) -> list:
     # SIP auth password
     if password_type.lower() == "sip":
         print("Generating new SIP passwords.")
-        new_passwords = api.get.sip_passwords_generate(len(users))["passwords"]
+        new_passwords = api.password_generate.get_sip_passwords_generate(len(users))[
+            "passwords"
+        ]
         users_and_new_passwords = list(zip(users, new_passwords))
 
         print("Setting new SIP passwords.")
         for user in users_and_new_passwords:
-            api.put.user_authentication_service(user[0], user[1])
+            api.authentication.put_user_authentication_service(user[0], user[1])
 
         print("Setting new SIP passwords complete.")
-        formatted_user_and_new_passwords = [
+        return [
             {"userId": user[0], "newPassword": user[1]}
             for user in users_and_new_passwords
         ]
-        return formatted_user_and_new_passwords
 
     # intigration password
     elif password_type.lower() == "web":
         print("Generating new SIP passwords.")
-        new_passwords = api.get.passwords_generate(
+        new_passwords = api.password_generate.get_passwords_generate(
             service_provider_id, group_id, len(users)
         )["passwords"]
         users_and_new_passwords = list(zip(users, new_passwords))
 
         print("Setting new SIP passwords.")
         for user in users_and_new_passwords:
-            api.put.user_web_authentication_password(user[0], user[1])
+            api.authentication.put_user_web_authentication_password(user[0], user[1])
 
         print("Setting new SIP passwords complete.")
-        formatted_user_and_new_passwords = [
+        return [
             {"userId": user[0], "newPassword": user[1]}
             for user in users_and_new_passwords
         ]
-        return formatted_user_and_new_passwords
 
-    # voicemail/ oortal
-    elif password_type.lower() == "vm":  # voicemail
+    # voicemail/ portal
+    elif password_type.lower() == "vm":
         print("Generating new voicemail passcodes.")
-        new_passcodes = api.get.passcodes_generate(
+        new_passcodes = api.password_generate.get_passcodes_generate(
             service_provider_id, group_id, len(users)
         )["passcodes"]
         users_and_new_passcodes = list(zip(users, new_passcodes))
 
         print("Setting new voicemail passcodes.")
         for user in users_and_new_passcodes:
-            api.put.user_portal_passcode(user[0], user[1])
+            api.users.put_user_portal_passcode(user[0], user[1])
 
         print("Setting new voicemail passcodes complete.")
-        formatted_user_and_new_passcodes = [
+        return [
             {"userId": user[0], "newPasscode": user[1]}
             for user in users_and_new_passcodes
         ]
-        return formatted_user_and_new_passcodes
 
-    # raise error if user attempts to change another type of password.
     else:
+        # raise error if user attempts to change another type of password.
         raise OSInvalidPasswordType
