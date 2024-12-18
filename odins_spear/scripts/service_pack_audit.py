b892cbb1a9ac06de6a534e2c0e574cae8ef31b8b
Jordan <jprescott23@gmail.com>
2024-12-18 10:12:43 +0000

    mir-scripts-service-pack-audit

--- odins_spear/scripts/service_pack_audit.py
+++ odins_spear/scripts/service_pack_audit.py
@@ -1,19 +1,14 @@
 from tqdm import tqdm
 
 
-def main(api, service_provider_id, group_id):
+def main(api, service_provider_id: str, group_id: str) -> dict:
     """
     A stripped down version of group audit focussing only on the service packs assigned within
     the group. This only shows the service packs assigned and total count of unlike group audit
     which details the users this is assigned to.
-
-    :param service_provider_id (str): Service Provider ID or Enterprise ID containing the Group ID.
-    :param group_id (str): Group ID to generate the report for.
-
-    :return str: A JSON formatted report of service packs assigned in the group.
     """
 
-    service_report = api.get.group_services(group_id, service_provider_id)
+    service_report = api.services.get_group_services(group_id, service_provider_id)
 
     assigned_service_pack_services = []
 
